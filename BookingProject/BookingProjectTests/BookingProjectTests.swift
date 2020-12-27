02e2279b7118f64198a1a3584594abe56c5b07a4
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-27 17:17:43 +0500

    1609071458

--- BookingProject/BookingProjectTests/BookingProjectTests.swift
+++ BookingProject/BookingProjectTests/BookingProjectTests.swift
@@ -2,16 +2,16 @@ import XCTest
 @testable import BookingProject
 
 class BookingProjectTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
     }
-
+    
     override func tearDownWithError() throws {
     }
-
+    
     func testExample() throws {
     }
-
+    
     func testPerformanceExample() throws {
         self.measure {
         }
