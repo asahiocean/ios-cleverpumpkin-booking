29f43f03abb2cddc29ebca3ab2fb879807067ee5
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-27 17:15:51 +0500

    1609071345

--- BookingProject/BookingProject/SceneDelegate.swift
+++ BookingProject/BookingProject/SceneDelegate.swift
@@ -1,9 +1,9 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         if let windowScene = scene as? UIWindowScene {
             self.window = UIWindow(windowScene: windowScene)
@@ -16,19 +16,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                         self.viewControllers = [tvc]
                     }
                 }
-
+                
                 window.rootViewController = NavigationController()
                 window.makeKeyAndVisible()
             }
         }
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) { }
     func sceneDidBecomeActive(_ scene: UIScene) { }
     func sceneWillResignActive(_ scene: UIScene) { }
     func sceneWillEnterForeground(_ scene: UIScene) { }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
