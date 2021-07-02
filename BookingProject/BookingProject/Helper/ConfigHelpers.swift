324c6f80b724e66c7b4f0b6188b6ad5ea2d2fd29
ASAHI OCEAN <asahiocean@icloud.com>
2021-07-02 17:19:45 +0500

    Update ConfigHelpers.swift

--- BookingProject/BookingProject/Helper/ConfigHelpers.swift
+++ BookingProject/BookingProject/Helper/ConfigHelpers.swift
@@ -1,10 +1,12 @@
 import Foundation
 import UIKit
 
+let screenBounds = UIScreen.main.bounds
+
 enum TableConfig {
-    static let width: CGFloat = UIScreen.main.bounds.width
-    static let rowHeight: CGFloat = UIScreen.main.bounds.height / 10
-    static let estimatedRowHeight: CGFloat = UIScreen.main.bounds.height / 10
+    static let width: CGFloat = screenBounds.width
+    static let rowHeight: CGFloat = screenBounds.height / 10
+    static let estimatedRowHeight: CGFloat = screenBounds.height / 10
     static let decelerationRate: UIScrollView.DecelerationRate = .fast
     static let autoresizingMask: UIView.AutoresizingMask =  [.flexibleWidth, .flexibleHeight]
 }
